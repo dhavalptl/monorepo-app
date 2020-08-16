00c31cec688a84395e756218deb38852b164eedf
Dhaval <dhvl.ptl123@yahoo.in>
2020-08-16 22:48:07 +0530

    added applications

---
+++ packages/application1/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from '@dhavalptl/uicomponents';
+const App: React.FC = () => {
+    return (
+        <div>
+            <header>
+                <h2>React Application</h2>
+                <Button title="Test" onClick={() => {
+                    console.log("Button Clicked");
+                }}/>
+            </header>
+        </div>
+    );
+}
+
+export default App;
